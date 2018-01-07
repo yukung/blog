c593cdcde0084e26f51ed81318632c88515f82f8
yukung <yukung.i@gmail.com>
2018-01-07 14:17:50 +0900

    Changed to be able to insert express middleware when app is started.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var ghost = require('ghost');
 var path = require('path');
+var express = require('express');
+var parentApp = express();
+var utils = require('./node_modules/ghost/core/server/services/url/utils');
 
 ghost().then(function (ghostServer) {
-    ghostServer.start();
+    parentApp.use(utils.getSubdir(), ghostServer.rootApp);
+    ghostServer.start(parentApp);
 });
