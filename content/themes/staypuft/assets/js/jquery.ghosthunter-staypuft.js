0f925cb0390cfcb9ea11b1b5f4cd9a72c5f04a53
yukung <yukung.i@gmail.com>
2018-05-26 17:03:53 +0900

    Tune up font family and font size.

--- content/themes/staypuft/assets/js/jquery.ghosthunter-staypuft.js
+++ content/themes/staypuft/assets/js/jquery.ghosthunter-staypuft.js
@@ -3,7 +3,7 @@ $("#search-field").ghostHunter({
     onKeyUp: true,
     result_template: "<li id='gh-{{ref}}' class='gh-search-item'><a href='{{link}}'>{{title}}</a></li>",
     info_template: "<header class='read-next-card-header'><h3 class='read-next-card-header-title'>Number of posts found: {{amount}}</h3></header>",
-    subpath: "/blog",
+    subpath: "",
     onComplete: function(results) {
         if (results && results.length) {
             $('.search-results').show();
